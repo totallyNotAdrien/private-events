0a293914060e0480119c26797bbd5c769986a53f
Tina Thompson <mark_junior13@yahoo.com>
2022-05-09 07:51:43 -0500

    add page to show user's created events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations"}
   resources :events
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
